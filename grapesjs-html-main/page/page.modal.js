e77109694dda8c587b33286f1ab0dc23e9af7546
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-10-04 09:50:16 +0400

    cccc
    cccc

--- grapesjs-html-main/page/page.modal.js
+++ grapesjs-html-main/page/page.modal.js
@@ -7,7 +7,7 @@ const Page = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      maxlength: 25,
+      maxlength: 100,
     },
     slug: {
       type: String,
