cb0c2ab044c30bc3a672e0e989ee33e97c07bd37
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-10-04 12:50:55 +0400

    bg changer
    bg changer

--- grapesjs-html-main/assets/assets.controller.js
+++ grapesjs-html-main/assets/assets.controller.js
@@ -2,15 +2,18 @@ export const loadAllAssets = async (req, res) => {
   const assets = [
     {
       type: 'image',
-      src: 'y.jpg',
+      src: 'C:/Users/DELL 5590/Desktop/Al Nakheel Logo.bmp',
+      height: 350,
+      width: 250,
+      name: 'displayName'
     },
     {
-      type: 'image',
-      src: 'y.jpg',
-    },
-    {
-      type: 'image',
-      src: 'z.jpg',
+      // As the 'image' is the base type of assets, omitting it will
+      // be set as `image` by default
+      src: 'https://static.wikia.nocookie.net/looneytunes/images/2/20/Daffy_Duck_%28SVG%29.svg/revision/latest?cb=20220111144139',
+      height: 350,
+      width: 250,
+      name: 'displayName'
     },
   ];
   res.json(assets);
--- grapesjs-html-main/page/page.modal.js
+++ grapesjs-html-main/page/page.modal.js
@@ -7,11 +7,7 @@ const Page = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-<<<<<<< HEAD
       maxlength: 100,
-=======
-      maxlength: 50,
->>>>>>> 032c6226595c9612656c5445398c3b2caac5e650
     },
     slug: {
       type: String,
