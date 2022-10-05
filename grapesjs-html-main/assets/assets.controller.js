edaaff26181331563b0bff4114a08e5dae5801e8
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-10-05 18:05:56 +0400

    changes
    changes

--- grapesjs-html-main/assets/assets.controller.js
+++ grapesjs-html-main/assets/assets.controller.js
@@ -2,10 +2,10 @@ export const loadAllAssets = async (req, res) => {
   const assets = [
     {
       type: 'image',
-      src: 'C:/Users/DELL 5590/Desktop/Al Nakheel Logo.bmp',
+      src: 'https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Fimage-removebg-preview%20(1).pngb1a05011-7bcb-453a-8681-4838ee6affa3?alt=media&token=8e57e074-a8e6-479f-8ffb-164c2f503732',
       height: 350,
       width: 250,
-      name: 'displayName'
+      name: 'sexy'
     },
     {
       // As the 'image' is the base type of assets, omitting it will
@@ -14,7 +14,7 @@ export const loadAllAssets = async (req, res) => {
       height: 350,
       width: 250,
       name: 'displayName'
-    },
+    },  
   ];
   res.json(assets);
 };
